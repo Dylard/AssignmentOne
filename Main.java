0ad80ed2b0921488bd021cd4c9f3e777a3624672
Dylan Kennedy <dylankennedy@comcast.net>
2024-07-21 21:47:10 -0600

    Added updated pseudocode and PersonInfo.java files

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+//start of program
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize variables
+        String firstName = "Daemon";
+        String lastName = "Kenth";
+        String streetAddress = "123 Main St";
+        String city = "Parker";
+        String zipCode = "89999";
+
+        // Print the information
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
+//end of program
